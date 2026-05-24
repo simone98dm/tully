a24fbc01e4f8730ebca6537c136627215651ff97
simone98dm <simone.dalmas@outlook.it>
2026-05-24 19:16:18 +0200

    Initial Commit

---
+++ tully/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tully
+//
+//  Created by Simone Dal Mas on 24/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tully/tullyApp.swift
@@ -0,0 +1,17 @@
+//
+//  tullyApp.swift
+//  tully
+//
+//  Created by Simone Dal Mas on 24/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct tullyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
